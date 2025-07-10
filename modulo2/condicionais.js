f1ecdc58e048b1306e3387ca45faea8909163f47
Monaliza Janine <39283836+Monaliza-Janine@users.noreply.github.com>
2025-07-09 21:57:13 -0300

    Add files via upload

---
+++ modulo2/condicionais.js
@@ -0,0 +1,8 @@
+let horas = 10;
+if(horas < 12){
+    console.log("Bom dia!");
+}else if(horas < 18){
+    console.log("Boa tarde");
+}else{
+    console.log("Boa noite")
+}
---
+++ modulo2/variaveis.js
@@ -0,0 +1,17 @@
+let idade = 25;
+const nome = "Monaliza";
+let endereco = "Natal/Rn";
+
+const anotherMessage = (`Olá, ${nome}`);
+
+const carro = {
+    marca: "Honda",
+    ano: 2022,
+    cor: "prata",
+};
+
+console.log(nome);
+console.log(endereco);
+console.log(idade, typeof idade);
+console.log(anotherMessage);
+console.log(carro.ano);
